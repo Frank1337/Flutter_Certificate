2f99fc54d43ad1d3a3f1f97ea99b818a8156c5b0
Frank Büttgenbach <FBuettgenbach@gmx.de>
2024-11-22 13:12:03 +0100

    Add more buttons to button project and played a little bit around with Wrap Widget

--- buttons_06/lib/main.dart
+++ buttons_06/lib/main.dart
@@ -6,8 +6,17 @@ main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var city = ["London", "Paris", "New York", "Tokyo", "Sydney"];
+
+  var firstCity = "London";
 
   @override
   Widget build(BuildContext context) {
@@ -20,27 +29,142 @@ class MyApp extends StatelessWidget {
             ),
             backgroundColor: Colors.blueAccent),
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Center(
-                child: SafeArea(
-                  child: Text(
-                    "Welcome to Franks Buttons App",
-                    style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        color: Colors.black,
-                        fontSize: 25.0),
+          child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+              child: Wrap(
+                direction: Axis.vertical,
+                alignment: WrapAlignment.start,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                spacing: 20.0,
+                children: [
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: constraints.maxWidth),
+                    child: OverflowBar(
+                      alignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            overlayColor:
+                                WidgetStateProperty.all<Color>(Colors.blue),
+                          ),
+                          child: const Text("Flights"),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            overlayColor:
+                                WidgetStateProperty.all<Color>(Colors.blue),
+                          ),
+                          child: const Text("Hotels"),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            overlayColor:
+                                WidgetStateProperty.all<Color>(Colors.blue),
+                          ),
+                          child: const Text("Cars"),
+                        )
+                      ],
+                    ),
                   ),
-                ),
+                  const SafeArea(
+                    child: Text(
+                      "Welcome to Franks Buttons App",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black,
+                          fontSize: 25.0),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blueAccent,
+                      shadowColor: Colors.red,
+                      elevation: 5,
+                    ),
+                    onPressed: () {
+                      print('elevated Button Pressed');
+                    },
+                    child: const Text(
+                      'Elevated Button',
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      print('Icon Button Pressed');
+                    },
+                    icon: const Icon(Icons.volume_up),
+                    tooltip: "Increase volume by 10",
+                  ),
+                  const Text("Sound"),
+                  TextButton(
+                    onPressed: () {
+                      print('Text Button Pressed');
+                    },
+                    style: ButtonStyle(
+                      foregroundColor:
+                          WidgetStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          WidgetStateProperty.all<Color>(Colors.greenAccent),
+                      shadowColor: WidgetStateProperty.all<Color>(Colors.black),
+                      elevation: WidgetStateProperty.all<double>(5),
+                      overlayColor: WidgetStateProperty.all<Color>(Colors.red),
+                      surfaceTintColor:
+                          WidgetStateProperty.all<Color>(Colors.red),
+                      animationDuration: const Duration(milliseconds: 10000),
+                    ),
+                    child: const Text(
+                      'Text Button',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                  DropdownButton<String>(
+                    items: city.map((String dropDownStringItem) {
+                      return DropdownMenuItem<String>(
+                        value: dropDownStringItem,
+                        child: Text(
+                          dropDownStringItem,
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String? newUserValue) {
+                      setState(() {
+                        firstCity = newUserValue ?? "London";
+                      });
+                    },
+                    value: firstCity,
+                    icon: const Icon(Icons.access_alarm),
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueAccent,
+                      backgroundColor: Colors.yellow,
+                    ),
+                    dropdownColor: Colors.green,
+                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  OutlinedButton(
+                      style: ButtonStyle(
+                          shape: WidgetStateProperty.all<OutlinedBorder>(
+                              const StadiumBorder())),
+                      onPressed: () {
+                        print('Outlined Button Pressed');
+                      },
+                      child: const Text('Outlined Button')),
+                ],
               ),
-              RaisedButton(
-                onPressed: () {
-                  print('Raised Button Pressed');
-                },
-                child: const Text('Raised Button'),
-              ),
-            ],
-          ),
+            );
+          }),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
